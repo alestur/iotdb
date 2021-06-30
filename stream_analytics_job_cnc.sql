d2f45607bf27ff9817fc82b4dfe204ac4c760418
Ales <ales@rlyeh.biz>
2021-06-30 15:22:34 +0200

    Stream analytics query update

--- stream_analytics_job_cnc.sql
+++ stream_analytics_job_cnc.sql
@@ -1,28 +1,12 @@
 SELECT [T].[DeviceID] AS [DeviceId]
     ,[T].[FactoryID] AS [FactoryId]
     ,System.TimeStamp AS [Tst]
-    ,CAST(CONCAT(
-        SUBSTRING([T].[DeviceTime], 1, 4), '-'
-        ,SUBSTRING([T].[DeviceTime], 5, 2), '-'
-        ,SUBSTRING([T].[DeviceTime], 7, 2), 'T'
-        ,SUBSTRING([T].[DeviceTime], 9, 2), ':'
-        ,SUBSTRING([T].[DeviceTime], 11, 2), ':'
-        ,SUBSTRING([T].[DeviceTime], 13, 6)
-    ) as datetime) AS [DeviceTime]
+    ,UDF.ParseDateTime([T].[DeviceTime]) AS [DeviceTime]
     ,[T].[DeviceStatus] AS [DeviceStatus]
-    ,CAST(CONCAT(
-        SUBSTRING([T].[MachineTime], 1, 4), '-'
-        ,SUBSTRING([T].[MachineTime], 5, 2), '-'
-        ,SUBSTRING([T].[MachineTime], 7, 2), 'T'
-        ,SUBSTRING([T].[MachineTime], 9, 2), ':'
-        ,SUBSTRING([T].[MachineTime], 11, 2), ':'
-        ,SUBSTRING([T].[MachineTime], 13, 6)
-    ) as datetime) AS [MachineTime]
+    ,UDF.ParseDateTime([T].[MachineTime]) AS [MachineTime]
     ,[T].[MachineState] AS [MachineState]
-    ,CAST(SUBSTRING([T].[RunTime], 1, LEN([T].[RunTime]) - 4) as float) * 3600
-        + CAST(SUBSTRING([T].[RunTime], LEN([T].[RunTime]) - 3, 2) as float) * 60
-        + CAST(SUBSTRING([T].[RunTime], LEN([T].[RunTime]) - 1, 2) as float) AS [RunTime]
-    ,DATEDIFF(second, '00:00:00', CAST(System.TimeStamp AS DATETIME)) - [__DeviceCNC__].[ShiftStartSec] AS [ShiftTime]
+    ,UDF.DayTimeToSec([T].[RunTime]) AS [RunTime]
+    ,DATEDIFF(second, '00:00:00', CAST(System.TimeStamp AS DATETIME)) - [__DeviceCNC__].[ShiftStartTimeSec] AS [ShiftTime]
     ,CAST([T].[SpindleLoad] as float) AS [SpindleLoad]
     ,CAST([T].[XAxisLoad] as float) AS [XAxisLoad]
     ,CAST([T].[YAxisLoad] as float) AS [YAxisLoad]
@@ -34,4 +18,5 @@ INTO
     [__TelemetryCNC__]
 FROM
     [__IoTHub__] [T]
-    INNER JOIN [__DeviceCNC__] ON [T].[DeviceID] = [__DeviceCNC__].[DeviceID]
+    INNER JOIN [__Device__] ON [T].[DeviceID] = [__Device__].[DeviceID]
+    INNER JOIN [__DeviceCNC__] ON [__Device__].[Id] = [__DeviceCNC__].[Device]
